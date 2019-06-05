18d5d8e844b6ebaeb5d3dfbc802d283ae783b1fe
Magomed Magomedov <alkaitagi@outlook.com>
2019-06-06 00:07:49 +0300

    Particle improvements

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -11,6 +11,16 @@ public class Projectile : MonoBehaviour
     private float damage;
     private float speed;
 
+    private Vector2 Position
+    {
+        get => rigidbody.position;
+        set
+        {
+            rigidbody.position = value;
+            transform.position = Position;
+        }
+    }
+
     private new Rigidbody2D rigidbody;
     private static readonly RaycastHit2D[] hits = new RaycastHit2D[20];
 
@@ -26,15 +36,15 @@ public class Projectile : MonoBehaviour
     private void FixedUpdate()
     {
         var distance = Time.fixedDeltaTime * speed;
-        var destination = transform.position + transform.up * distance;
 
         if (rigidbody.Cast(transform.up, hits, distance) > 0)
         {
-            rigidbody.MovePosition(hits[0].point);
+            Position += hits[0].fraction * (Vector2)transform.up;
+            TryDamage(hits[0].transform.gameObject);
             Destroy();
         }
         else
-            rigidbody.MovePosition(destination);
+            Position += distance * (Vector2)transform.up;
     }
 
     private void Destroy()
@@ -42,11 +52,11 @@ public class Projectile : MonoBehaviour
         trailEffect.transform.parent = null;
         Destroy(trailEffect, 2);
         Destroy(gameObject);
-        Instantiate(hitEffect, transform.position, transform.rotation);
+        Instantiate(hitEffect, Position, transform.rotation);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void TryDamage(GameObject target)
     {
-        Destroy();
+
     }
 }
