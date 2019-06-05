4ee46b56a9ff27b3223c16c5d9e817d911fb7e59
Magomed Magomedov <alkaitagi@outlook.com>
2019-06-05 23:46:04 +0300

    More lightning

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,26 +6,42 @@ public class Projectile : MonoBehaviour
     [SerializeField]
     private GameObject hitEffect;
     [SerializeField]
-    private Transform trailEffect;
+    private GameObject trailEffect;
 
     private float damage;
     private float speed;
 
     private new Rigidbody2D rigidbody;
+    private static readonly RaycastHit2D[] hits = new RaycastHit2D[20];
 
     private void Awake() => rigidbody = GetComponent<Rigidbody2D>();
 
     public void Launch(float damage, float distance, float speed)
     {
         this.damage = damage;
-        rigidbody.AddForce(speed * transform.up, ForceMode2D.Impulse);
+        this.speed = speed;
         Invoke("Destroy", distance / speed);
+    }
+
+    private void FixedUpdate()
+    {
+        var distance = Time.fixedDeltaTime * speed;
+        var destination = transform.position + transform.up * distance;
+
+        if (rigidbody.Cast(transform.up, hits, distance) > 0)
+        {
+            rigidbody.MovePosition(hits[0].point);
+            Destroy();
+        }
+        else
+            rigidbody.MovePosition(destination);
     }
 
     private void Destroy()
     {
-        Destroy(gameObject);
         trailEffect.transform.parent = null;
+        Destroy(trailEffect, 2);
+        Destroy(gameObject);
         Instantiate(hitEffect, transform.position, transform.rotation);
     }
 
