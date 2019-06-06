b58319ea2e04608b7515ea74be86db94ca5ca26b
Magomed Magomedov <alkaitagi@outlook.com>
2019-06-06 20:29:09 +0300

    Simplify Warp scene

--- Assets/Scripts/WarpManager.cs
+++ Assets/Scripts/WarpManager.cs
@@ -13,8 +13,6 @@ public class WarpManager : MonoBehaviour
     [SerializeField]
     private GameObject effects;
     [SerializeField]
-    private NoiseSettings cameraNoise;
-    [SerializeField]
     private CanvasToggle canvasToggle;
 
     private void Awake()
@@ -32,31 +30,28 @@ public class WarpManager : MonoBehaviour
     private IEnumerator Transition(Gate gate)
     {
         effects.SetActive(true);
-        var noise = CameraManager.VirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
         var player = FindObjectOfType<Player>();
         player.enabled = false;
 
-        canvasToggle.IsVisible = true;
-        yield return new WaitForSeconds(1);
-        canvasToggle.IsVisible = false;
+        yield return StartCoroutine(Fade());
 
         gate.OnWarp.Invoke();
-
-        noise.m_NoiseProfile = cameraNoise;
         player.transform.position = transform.position;
         player.transform.eulerAngles = new Vector3(0, 0, -45);
 
         yield return new WaitForSeconds(duration);
 
-        canvasToggle.IsVisible = true;
-        yield return new WaitForSeconds(1);
-        canvasToggle.IsVisible = false;
+        yield return StartCoroutine(Fade());
 
         player.enabled = true;
         player.transform.position = gate.Destination.position;
-
-        noise.m_NoiseProfile = null;
-        CameraManager.Main.transform.eulerAngles = Vector3.zero;
         effects.SetActive(false);
+    }
+
+    private IEnumerator Fade()
+    {
+        canvasToggle.IsVisible = true;
+        yield return new WaitForSeconds(1);
+        canvasToggle.IsVisible = false;
     }
 }
