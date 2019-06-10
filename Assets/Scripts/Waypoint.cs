14a29a3019e6d5569f303a6e090e8de365b1422c
Magomed Magomedov <alkaitagi@outlook.com>
2019-06-10 17:15:19 +0300

    Ghost improvements

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -24,6 +24,7 @@ public class Waypoint : MonoBehaviour
             .Where(c => c.GetComponent<Waypoint>())
             .Select(c => c.transform)
             .Where(t => t != transform)
+            .Where(t => !Physics2D.Linecast(transform.position, t.position, LayerMask.GetMask("Environment")))
         );
     }
 }
