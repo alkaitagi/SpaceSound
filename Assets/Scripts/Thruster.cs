a8f90be63019ecb362dc54707c76a1f60c3f7706
Magomed Magomedov <alkaitagi@outlook.com>
2019-06-17 12:15:34 +0300

    Add Thruster cooldown

--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -5,15 +5,28 @@ public class Thruster : MonoBehaviour
     [SerializeField]
     private float force;
     [SerializeField]
+    private float cooldown;
+
+    [Space(10)]
+    [SerializeField]
     private GameObject effect;
 
     private new Rigidbody2D rigidbody;
 
     private void Awake() => rigidbody = GetComponentInParent<Rigidbody2D>();
 
+    private bool isReady = true;
+    private void Ready() => isReady = true;
+
     public void Burst()
     {
-        rigidbody.AddForce(force * transform.up, ForceMode2D.Impulse);
-        Instantiate(effect, transform.position, transform.rotation);
+        if (isReady)
+        {
+            isReady = false;
+            Invoke("Ready", cooldown);
+
+            rigidbody.AddForce(force * transform.up, ForceMode2D.Impulse);
+            Instantiate(effect, transform.position, transform.rotation);
+        }
     }
 }
