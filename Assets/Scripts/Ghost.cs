64a0a9895c1de8c94cb8b47eca8a10f027cbb7f9
Magomed Magomedov <alkaitagi@outlook.com>
2019-06-25 08:21:56 +0300

    Physics improvements

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,24 +1,31 @@
-using System.Linq;
-
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Ghost : MonoBehaviour
 {
     [SerializeField]
     private Range speed;
 
     private Transform target;
+    private Rigidbody2D rigidbody;
 
-    private void Awake() => speed.Evaluate();
+    private void Awake()
+    {
+        speed.Evaluate();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (target)
-            transform.position = Vector2.MoveTowards
+            rigidbody.MovePosition
             (
-                transform.position,
-                target.position,
-                speed.Random() * Time.smoothDeltaTime
+                Vector2.MoveTowards
+                (
+                    transform.position,
+                    target.position,
+                    speed.Random() * Time.fixedDeltaTime
+                )
             );
     }
 
