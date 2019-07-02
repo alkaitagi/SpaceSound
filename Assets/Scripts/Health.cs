16b7ca874186c4a019db414d664d7eb6f7b0cccb
Magomed Magomedov <alkaitagi@outlook.com>
2019-07-02 11:20:38 +0300

    Add player start invulnerability

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -12,16 +12,26 @@ public class Health : MonoBehaviour
     private UnitType type;
     public UnitType Type => type;
     [SerializeField]
+    private float startInvulnerability;
+
+    [Space(10)]
+    [SerializeField]
     private GameObject effect;
     [SerializeField]
     private VoidEvent onDestroy;
     public VoidEvent OnDestroy => onDestroy;
 
+    private void Update() =>
+        startInvulnerability = Mathf.Max(0, startInvulnerability - Time.deltaTime);
+
     public void Destroy()
     {
-        if (effect)
-            Instantiate(effect, transform.position, transform.rotation);
-        OnDestroy.Invoke();
-        Destroy(gameObject);
+        if (startInvulnerability == 0)
+        {
+            if (effect)
+                Instantiate(effect, transform.position, transform.rotation);
+            OnDestroy.Invoke();
+            Destroy(gameObject);
+        }
     }
 }
