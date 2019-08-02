405189b5e2d8cd9bf220120805948bde6c31a956
Magomed Magomedov <alkaitagi@outlook.com>
2019-08-02 17:48:10 +0300

    Add min length for input fields

--- Assets/UI/Collectors/InputFieldCollector.cs
+++ Assets/UI/Collectors/InputFieldCollector.cs
@@ -8,9 +8,15 @@ public class InputFieldCollector : BaseCollector
 {
     private InputField inputField;
 
+    [Space(10)]
+    [SerializeField]
+    private int minLength;
+
     private void Awake() => inputField = GetComponent<InputField>();
 
-    protected override bool Validate() => !string.IsNullOrEmpty(inputField.text);
+    protected override bool Validate() =>
+        !string.IsNullOrEmpty(inputField.text)
+        && (minLength <= 0 || inputField.text.Length >= minLength);
 
     protected override JToken Read() => new JValue(inputField.text);
 }
